3a9c6249d7ed9d492f7bc015c6798703395ee77d
Cristine Sutil <43651678+CristineSutil@users.noreply.github.com>
2020-04-13 10:30:17 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,43 +6,54 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 STATE1 = 1
 STATE2 = 2
+#STATE3 = 3
+#STATE4 = 4
 
 def welcome(update, context):
     try:
         username = update.message.from_user.username
         firstName = update.message.from_user.first_name
         lastName = update.message.from_user.last_name
-        message = 'Olá, ' + firstName + '!'
+        message = 'Olá, ' + firstName + '!' + 'digite /feedback para dizer o que achou do nosso tutorial'
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     except Exception as e:
         print(str(e))
 
 
-def feedback(update, context):
+def coronaquestions(update, context):
     try:
-        message = 'Por favor, digite um feedback para o nosso tutorial:'
+        message = """Olá sou Drinha Aninha Bot, Estou aqui pra te ajudar? \n 
+            Escolha uma opção abaixo para começarmos: \n
+            1 - O que é Coronavirus\n
+            2 - Como podemos nos previnir\n
+            3 - Você sabia\n"""
         update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
         return STATE1
     except Exception as e:
         print(str(e))
 
 
-def inputFeedback(update, context):
-    feedback = update.message.text
-    print(feedback)
-    if len(feedback) < 10:
-        message = """Seu feedback foi muito curtinho... 
-                        \nInforma mais pra gente, por favor?"""
+def inputCoronaquestions(update, context):
+    coronaquestions = update.message.text
+    print(coronaquestions)
+    if coronaquestions == "1" 
+    or coronaquestions == "O que é Coronavirus"
+    or coronaquestions =="Coronavirus"
+    or coronaquestions == "virus":
+        message = """colocar o que eu quero na resposta 1"""
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE1
+        return STATE2
     else:
-        message = "Muito obrigada pelo seu feedback!"
+        if coronaquestions == "2" 
+    or coronaquestions == "Como podemos nos previnir"
+    or coronaquestions =="prevenção"
+    or coronaquestions == "previnir":
+        message = " colocar o que eu quero responder na numero 2 "
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return STATE2
 
-
-def inputFeedback2(update, context):
-    feedback = update.message.text
-    message = "Muito obrigada pelo seu feedback!"
+def inputCoronaquestions2(update, context):
+    message = "Muito obrigada! Em breve teremos novidades :)"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
@@ -59,15 +70,15 @@ def main():
         updater.dispatcher.add_handler(CommandHandler('start', welcome))
 
         conversation_handler = ConversationHandler(
-            entry_points=[CommandHandler('feedback', feedback)],
+            entry_points=[CommandHandler('coronaquestions', coronaquestions)],
             states={
-                STATE1: [MessageHandler(Filters.text, inputFeedback)],
-                STATE2: [MessageHandler(Filters.text, inputFeedback2)]
+                STATE1: [MessageHandler(Filters.text, inputCoronaquestions)],
+                STATE2: [MessageHandler(Filters.text, inputCoronaquestions2)]
             },
             fallbacks=[CommandHandler('cancel', cancel)])
         updater.dispatcher.add_handler(conversation_handler)
 
-        print("Updater no ar1: " + str(updater))
+        print(str(updater))
         updater.start_polling()
         updater.idle()
     except Exception as e:
